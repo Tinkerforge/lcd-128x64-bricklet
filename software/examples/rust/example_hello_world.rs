03bd5c6e580528ac24fefdcbcbe82f20ed3cb01d
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:23:34 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_hello_world.rs
+++ software/examples/rust/example_hello_world.rs
@@ -1,23 +1,23 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, lcd_128x64_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, lcd_128x64_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your LCD 128x64 Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your LCD 128x64 Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let lcd_128x64_bricklet = LCD128x64Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let lcd = Lcd128x64Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Clear display
-    lcd_128x64_bricklet.clear_display();
+    lcd.clear_display();
 
     // Write "Hello World" starting from upper left corner of the screen
-    lcd_128x64_bricklet.write_line(0, 0, "Hello World".to_string());
+    lcd.write_line(0, 0, "Hello World".to_string());
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_touch.rs
+++ software/examples/rust/example_touch.rs
@@ -1,54 +1,56 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, lcd_128x64_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, lcd_128x64_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your LCD 128x64 Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your LCD 128x64 Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let lcd_128x64_bricklet = LCD128x64Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let lcd = Lcd128x64Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for touch position events.
-    let touch_position_listener = lcd_128x64_bricklet.get_touch_position_receiver();
-    // Spawn thread to handle received events. This thread ends when the lcd_128x64_bricklet
+    // Create receiver for touch position events.
+    let touch_position_receiver = lcd.get_touch_position_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `lcd` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in touch_position_listener {
-            println!("Pressure: {}", event.pressure);
-            println!("X: {}", event.x);
-            println!("Y: {}", event.y);
-            println!("Age: {}", event.age);
+        for touch_position in touch_position_receiver {
+            println!("Pressure: {}", touch_position.pressure);
+            println!("X: {}", touch_position.x);
+            println!("Y: {}", touch_position.y);
+            println!("Age: {}", touch_position.age);
             println!();
         }
     });
 
-    //Create listener for touch gesture events.
-    let touch_gesture_listener = lcd_128x64_bricklet.get_touch_gesture_receiver();
-    // Spawn thread to handle received events. This thread ends when the lcd_128x64_bricklet
+    // Create receiver for touch gesture events.
+    let touch_gesture_receiver = lcd.get_touch_gesture_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `lcd` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in touch_gesture_listener {
-            println!("Gesture: {}", event.gesture);
-            println!("Duration: {}", event.duration);
-            println!("Pressure Max: {}", event.pressure_max);
-            println!("X Start: {}", event.x_start);
-            println!("X End: {}", event.x_end);
-            println!("Y Start: {}", event.y_start);
-            println!("Y End: {}", event.y_end);
-            println!("Age: {}", event.age);
+        for touch_gesture in touch_gesture_receiver {
+            println!("Gesture: {}", touch_gesture.gesture);
+            println!("Duration: {}", touch_gesture.duration);
+            println!("Pressure Max: {}", touch_gesture.pressure_max);
+            println!("X Start: {}", touch_gesture.x_start);
+            println!("X End: {}", touch_gesture.x_end);
+            println!("Y Start: {}", touch_gesture.y_start);
+            println!("Y End: {}", touch_gesture.y_end);
+            println!("Age: {}", touch_gesture.age);
             println!();
         }
     });
 
-    // Set period for touch position callback to 0.1s (100ms)
-    lcd_128x64_bricklet.set_touch_position_callback_configuration(100, true);
+    // Set period for touch position callback to 0.1s (100ms).
+    lcd.set_touch_position_callback_configuration(100, true);
 
-    // Set period for touch gesture callback to 0.1s (100ms)
-    lcd_128x64_bricklet.set_touch_gesture_callback_configuration(100, true);
+    // Set period for touch gesture callback to 0.1s (100ms).
+    lcd.set_touch_gesture_callback_configuration(100, true);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
